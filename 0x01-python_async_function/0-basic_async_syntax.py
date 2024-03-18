bfa74fa64966ae27a30ac905d13ecd49469c1f43
Neivanny1 <nevileneivanny@gmail.com>
2024-03-18 10:46:00 +0000

    fixed pycodestyle erros

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,7 @@ named wait_random that waits for a random delay between 0 and max_delay
 import random
 import asyncio
 
+
 async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
