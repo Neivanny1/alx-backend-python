420decf54b1d328ccc3a322372a032a1cf0c064b
Neivanny1 <nevileneivanny@gmail.com>
2024-03-18 11:57:21 +0000

    functin that measure executrion time of another function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""
-measure_time function with integers n and max_delay as arguments
-that measures the total execution time for wait_n(n, max_delay),
-and returns total_time / n.
-Return a float.
-"""
-from asyncio import run
-import random
+''' Description: Create a measure_time function with integers n and
+                 max_delay as arguments that measures the total execution
+                 time for wait_n(n, max_delay), and returns total_time / n.
+                 Your function should return a float.
+    Arguments: n: int, max_delay: int
+'''
+
 from time import time
+from asyncio import run
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
