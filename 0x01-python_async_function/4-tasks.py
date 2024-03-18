76c0e8c6155c267d02fa44ea82e1c2f8edecae9e
Neivanny1 <nevileneivanny@gmail.com>
2024-03-18 12:38:15 +0000

    fixed pycodestyle erros

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -14,12 +14,15 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    '''
-    Execute task_wait_random and returns sorted list of delay
-    '''
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = []
-    for task in tasks:
-        await task
-        delays.append(task.result())
-    return delays
+    '''Execute task_wait_random and returns sorted list of delay'''
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
+
+    for spawn in spawn_ls:
+        await spawn
+
+    return delay_ls
