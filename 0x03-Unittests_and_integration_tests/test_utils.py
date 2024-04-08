da5a36d0717492a8726c69c2550431a14a174158
Neivanny1 <nevileneivanny@gmail.com>
2024-04-08 06:21:19 +0000

    Define the TestGetJson(unittest.TestCase) class and implement the TestGetJson.test_get_json method to test that utils.get_json returns the expected result

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,6 @@ method to test that the method returns what it is supposed to.
 Decorate the method with @parameterized.expand
 to test the function for following inputs
 '''
-
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -16,12 +15,13 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({}, ("a",), "Key 'a' not found in nested map"),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in nested map")
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+    def test_access_nested_map_exception(self, nested_map, path, exp_err):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), exp_err)
 
 
 if __name__ == "__main__":
